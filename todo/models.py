3d39cfa23ce25ffb51b7de6620dceb9c9ee7323b
Mavlon2010 <143276310+Mavlon2010@users.noreply.github.com>
2025-02-01 21:59:22 +0500

    CRUD added

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,11 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+
+    def __str__(self):
+        return F"{self.title}"
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+
+from todo.models import Task
+from todo.serializers import TaskSerializer
+
 
 # Create your views here.
+class TaskViewSet(ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
